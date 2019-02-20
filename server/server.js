45b257fe453bf0e2282dde1a4779f0088bb45f92
SoHyun Kim <ruixijiejie@gmail.com>
2019-02-20 14:34:27 +0800

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+const mysql = require('mysql');
+const co = require('co-mysql');
+const body = require('koa-better-body');
+
+let conn = mysql.createPool({
+    host: '192.168.116.128',
+    user: 'root',
+    password: '123456',
+    port: 3306,
+    database: 'ydc'
+});
+
+let server = new Koa();
+server.listen(8081);
+
+let obj = body({uploadDir: 'upload'});
+server.use(obj);
+
+server.context.db = co(conn);
+
+server.use(async (ctx, next) => {
+    ctx.set('Access-Control-Allow-Origin', '*');
+
+    await next();
+});
+
+let router = new Router();
+
+router.use('/api', require('./routers/api'));
+
+server.use(router.routes());
